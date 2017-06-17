650dc6969ae8f80c7ff4b222bd5fa8c666d07c23
caoyongzheng <15267056106@139.com>
2017-06-17 12:48:53 +0800

    proxy fix query parse

--- services/proxy/proxy.go
+++ services/proxy/proxy.go
@@ -11,10 +11,12 @@ import (
 func init() {
 	env.R.Any("/proxy", func(w http.ResponseWriter, r *http.Request) {
 		u := r.URL.Query().Get("url")
+		r.URL.Query().Del("url")
 		remote, err := url.Parse(u)
 		if err != nil {
 			panic(err)
 		}
+		remote.RawQuery = remote.Query().Encode()
 		r.URL.RawQuery = ""
 		r.URL.Path = ""
 		proxy := httputil.NewSingleHostReverseProxy(remote)
