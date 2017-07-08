a252021ab9113e16f0076393296f3273568018ab
caoyongzheng <15267056106@139.com>
2017-07-08 18:13:44 +0800

    cors

--- handles/proxy.go
+++ handles/proxy.go
@@ -2,21 +2,31 @@ package handles
 
 import (
 	"io/ioutil"
+	"log"
 	"net/http"
+	"time"
 )
 
 // HandleProxy 反向代理
 func HandleProxy(w http.ResponseWriter, r *http.Request) {
-	url := r.URL.Query().Get("url")
-	if url == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+	w.Header().Set("Access-Control-Max-Age", string(3600*24*30*time.Second))
+	switch r.Method {
+	case http.MethodGet:
+		url := r.URL.Query().Get("url")
+		log.Println(url)
+		if url == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		data, _ := ioutil.ReadAll(resp.Body)
+		w.Write(data)
+	default:
 	}
-	resp, err := http.Get(url)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	data, _ := ioutil.ReadAll(resp.Body)
-	w.Write(data)
 }
